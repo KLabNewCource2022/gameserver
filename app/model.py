e97b0306381d0bc9767a2ca3edbc2bab75d36d45
Hiro-Onozawa <48467312+Hiro-Onozawa@users.noreply.github.com>
2022-07-13 02:59:20 +0000

    _get_user_by_token 関数を実装

--- app/model.py
+++ app/model.py
@@ -42,8 +42,14 @@ def create_user(name: str, leader_card_id: int) -> str:
 
 
 def _get_user_by_token(conn, token: str) -> Optional[SafeUser]:
-    # TODO: 実装
-    pass
+    result = conn.execute(
+        text("SELECT * FROM `user` WHERE token=:token"), {"token": token}
+    )
+    try:
+        row = result.one()
+    except NoResultFound:
+        return None
+    return SafeUser.from_orm(row)
 
 
 def get_user_by_token(token: str) -> Optional[SafeUser]:
