497a0ed5753243b715178d085a09854eb95061d6
onozawa-h <onozawa-h@klab.com>
2022-07-14 05:03:12 +0000

    model.py にそのルームのクライアントを一人取得する関数を実装

--- app/model.py
+++ app/model.py
@@ -246,6 +246,19 @@ def _delete_user_from_room_member(conn, room_id: int, token: str) -> None:
     )
 
 
+def _find_first_room_client_user(conn, room_id: int) -> Optional[str]:
+    result = conn.execute(
+        text(
+            "SELECT token FROM `room_member` WHERE room_id = :room_id AND is_host = 0"
+        ),
+        {"room_id": room_id},
+    )
+    try:
+        return result.one().token
+    except NoResultFound:
+        return None
+
+
 def leave_room(room_id: int, token: str) -> None:
     """ルームから退場する"""
     with engine.begin() as conn:
